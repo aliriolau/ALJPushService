84e3fbe2b49ecbba4ca6c8819cfb8bf29cd0adf6
Alirio.Lau <alirio.lau@gmail.com>
2020-03-28 14:26:29 +0800

    Initial Commit

---
+++ ALJPushService/ALJPushService.h
@@ -0,0 +1,30 @@
+//
+//                            Open Your Mind!
+//
+//                  .-~~~~~~~~~-._       _.-~~~~~~~~~-.
+//              __.'              ~.   .~              `.__
+//            .'//                  \./                  \\`.
+//          .'//                     |                     \\`.
+//        .'// .-~"""""""~~~~-._     |     _,-~~~~"""""""~-. \\`.
+//      .'//.-"                 `-.  |  .-'                 "-.\\`.
+//    .'//______.============-..   \ | /   ..-============.______\\`.
+//  .'______________________________\|/______________________________`.
+//
+//  ALJPushService.h
+//  ALJPushService
+//
+//  Created by Alirio Lau on 2020/3/28.
+//  Copyright © 2020 com.alirio.lau.www. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALJPushService.
+FOUNDATION_EXPORT double ALJPushServiceVersionNumber;
+
+//! Project version string for ALJPushService.
+FOUNDATION_EXPORT const unsigned char ALJPushServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALJPushService/PublicHeader.h>
+
+
